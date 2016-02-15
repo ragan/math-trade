7e78a36a7ce672b436a4f5fd467747772952b67b
Karol <peek.karol@gmail.com>
2016-02-15 21:06:56 +0100

    init

---
+++ src/main/java/trade/math/MtApplication.java
@@ -0,0 +1,11 @@
+package trade.math;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MtApplication.class, args);
+    }
+}
