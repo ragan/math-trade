f8bacffdb5506b3ba27d420f7451c1f101ed70d0
Karol <peek.karol@gmail.com>
2016-02-17 21:56:46 +0100

    todo

--- src/main/java/trade/math/service/SimpleBggIdToTitleService.java
+++ src/main/java/trade/math/service/SimpleBggIdToTitleService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class SimpleBggIdToTitleService implements BggIdToTitleService {
+    //TODO: use actual bggRepo service
     @Override
     public String getTitle(int bggId) {
         return "title";
