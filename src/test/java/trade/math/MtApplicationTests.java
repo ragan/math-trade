39fb253718335ab86ce092dc1c1a88dabf260841
Karol <peek.karol@gmail.com>
2016-02-22 20:28:30 +0100

    bcrypt when adding user

--- src/test/java/trade/math/MtApplicationTests.java
+++ src/test/java/trade/math/MtApplicationTests.java
@@ -3,41 +3,70 @@ package trade.math;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.http.MediaType;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import trade.math.controller.MainController;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MtApplication.class)
+@SpringApplicationConfiguration(classes = {MtApplication.class, SecurityConfiguration.class})
+@WebAppConfiguration
 @ActiveProfiles("test")
 public class MtApplicationTests {
+
+    @Autowired
+    WebApplicationContext context;
+
+    @Autowired
+    private FilterChainProxy filterChainProxy;
 
     private MockMvc mockMvc;
 
     @Before
     public void setUp() {
-        mockMvc = MockMvcBuilders.standaloneSetup(new MainController()).build();
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .dispatchOptions(true)
+                .addFilter(filterChainProxy)
+                .build();
     }
 
     @Test
-    public void testEverySiteEndPoint() throws Exception {
-//        this.mockMvc.perform(get("/")
-//                .accept(MediaType.TEXT_HTML))
-//                .andExpect(status().isOk());
-
+    public void testIndexPage() throws Exception {
+        this.mockMvc.perform(get("/")
+                .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void testSite() throws Exception {
-//        this.mockMvc.perform(get("/signUp")
-//                .accept(MediaType.TEXT_HTML))
-//                .andExpect(status().isOk());
+    public void testLoginPage() throws Exception {
+        this.mockMvc.perform(get("/login")
+                .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testSignUpPage() throws Exception {
+        this.mockMvc.perform(get("/login")
+                .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testAddItemPage() throws Exception {
+        this.mockMvc.perform(get("/addItem")
+                .accept(MediaType.TEXT_HTML))
+                .andExpect(status().isFound());
     }
 }
