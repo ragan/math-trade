7216e0689f7a58013bd2a48210ad9a17aa51db28
Karol <peek.karol@gmail.com>
2016-02-27 16:38:28 +0100

    delete query

--- src/main/java/trade/math/repository/TradeBoardGameRepository.java
+++ src/main/java/trade/math/repository/TradeBoardGameRepository.java
@@ -1,15 +1,7 @@
 package trade.math.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import trade.math.model.TradeBoardGame;
 
-import java.util.List;
-
 public interface TradeBoardGameRepository extends JpaRepository<TradeBoardGame, Long> {
-
-    @Query(value = "SELECT NAMES FROM TRADE_BOARD_GAME_NAMES WHERE NAMES LIKE %:name%", nativeQuery = true)
-    List<TradeBoardGame> findByName(@Param(value = "name") String name);
-
 }
