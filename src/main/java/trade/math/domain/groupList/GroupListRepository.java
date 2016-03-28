08e58efa48bc578122a3711969840adca95c3232
Karol <peek.karol@gmail.com>
2016-03-28 12:13:04 +0200

    repository

---
+++ src/main/java/trade/math/domain/groupList/GroupListRepository.java
@@ -0,0 +1,6 @@
+package trade.math.domain.groupList;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GroupListRepository extends JpaRepository<GroupList, Long> {
+}
